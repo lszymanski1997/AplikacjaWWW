87648fef9d986e0d3a81f43c176f8a2fd1551897
l.szymanski1997 <l.szymanski1997@wp.pl>
2022-10-12 14:03:20 +0200

    database integration

--- Projekt/sample/views.py
+++ Projekt/sample/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import User
+
+
+def index(request):
+    return HttpResponse(User.objects.get(id=1))
