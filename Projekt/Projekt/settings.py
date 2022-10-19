21c72dbc65cdab9ede6169d4016486fcd2a065e7
lszymanski1997 <56386201+lszymanski1997@users.noreply.github.com>
2022-10-19 14:05:30 +0200

    Update settings.py

--- Projekt/Projekt/settings.py
+++ Projekt/Projekt/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sample.apps.SampleConfig'
 ]
 
 MIDDLEWARE = [
@@ -119,5 +120,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
